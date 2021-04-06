74056e137cc4f4c1191f4887b98d6faca28edc84
Christopher Breen <C.jbreen61@gmail.com>
2021-04-06 10:54:36 -0400

    migrated and seeded db

---
+++ db/schema.rb
@@ -0,0 +1,63 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_04_06_143645) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string "name"
+    t.string "city"
+    t.string "grade"
+    t.integer "region_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["region_id"], name: "index_accounts_on_region_id"
+  end
+
+  create_table "inventoried_products", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "inventory_id", null: false
+    t.float "quantity"
+    t.float "cost"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["inventory_id"], name: "index_inventoried_products_on_inventory_id"
+    t.index ["product_id"], name: "index_inventoried_products_on_product_id"
+  end
+
+  create_table "inventories", force: :cascade do |t|
+    t.string "name"
+    t.date "date"
+    t.integer "account_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["account_id"], name: "index_inventories_on_account_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.float "price"
+    t.string "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "regions", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "accounts", "regions"
+  add_foreign_key "inventoried_products", "inventories"
+  add_foreign_key "inventoried_products", "products"
+  add_foreign_key "inventories", "accounts"
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Region.create(name: "MSD")
+Account.create(name:"Middle brook highschool", city:"mississippi", grade:"k-12", region_id:"1")
+Inventory.create(name:"May monthly", date:"05/01/2021", account_id: "1")
+Product.create(name:"apple" ,price: "0.50", category:"fruit")
+InventoriedProduct.create(inventory_id: "1", product_id: "1", quantity: "4", cost: "2.00")
